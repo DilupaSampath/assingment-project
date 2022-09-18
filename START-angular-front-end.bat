0fcfcdd25f7cf45011dedf2d288bbdf225e7db93
Dilupa Sampath <madsampath94@gmail.com>
2022-09-19 03:19:26 +0530

    First commit: Updated all projects with unit tests and code refactoring

--- START-angular-front-end.bat
+++ START-angular-front-end.bat
@@ -1,7 +1,8 @@
+@echo off
 call cd city-list-project
-
-IF NOT EXIST node_modules npm i
+call npm i
 call node node_modules/@angular/cli/bin/ng version
+echo please wait...
 call node --max_old_space_size=8192 node_modules/@angular/cli/bin/ng serve --proxy-config=proxy/local.conf.json
 
 pause
